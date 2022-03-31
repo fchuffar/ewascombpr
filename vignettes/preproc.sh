407001a13c71e55e703455df80aaa79ec75c2023
Florent Chuffart <florent.chuffart@gmail.com>
2022-03-31 09:19:53 +0200

    Work in progress on merging feature

--- vignettes/preproc.sh
+++ vignettes/preproc.sh
@@ -3,11 +3,12 @@ source config
 echo ${study}
 echo ${project}
 rsync -auvP ~/projects/${project}/results/${study}/ cargo:~/projects/${project}/results/${study}/
-  
+
 # launch default pipeline
 snakemake -s wf.py -pn
 
 # launch default pipeline
+cp preproc.sh 00_preproc.sh
 cp wf.py 00_local_wf.py
 cp rules.py 00_local_rules_.py
 
